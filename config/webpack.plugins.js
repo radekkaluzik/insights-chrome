74654241febacc74eb6b5dcd79798c5fd3c3eff1
Karel Hala <khala@redhat.com>
2021-07-06 16:14:16 +0200

    Add styling

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -43,7 +43,7 @@ const plugins = [
   ChunkMapper,
   new HtmlWebpackPlugin({
     template: path.resolve(__dirname, '../src/pug/templates/index.pug'),
-    inject: true,
+    inject: 'body',
     filename: '../index.html',
   }),
 ];
