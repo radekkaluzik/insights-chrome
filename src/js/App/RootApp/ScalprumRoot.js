5c1de01e3847fa13b82215079b7dd7e520c3fed3
Radek Kalužík <radek.kaluzik@email.cz>
2021-11-11 13:41:59 +0100

    Added chromeHistory to ScalprumProvider

--- src/js/App/RootApp/ScalprumRoot.js
+++ src/js/App/RootApp/ScalprumRoot.js
@@ -98,6 +98,7 @@ const ScalprumRoot = ({ config, ...props }) => {
               toggle: setActiveQuickStartID,
               Catalog: LazyQuickStartCatalog,
             },
+            chromeHistory: history,
           },
         }}
       >
